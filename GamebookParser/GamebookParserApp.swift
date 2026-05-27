e17ae41d2a5367ec9d9ddc2fe1fb8023e7d13099
Дмитрий Добрышин <dimkainc@gmail.com>
2026-05-27 14:44:02 +0300

    Initial Commit

---
+++ GamebookParser/GamebookParserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamebookParserApp.swift
+//  GamebookParser
+//
+//  Created by Дима on 27.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct GamebookParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
